512397e7522e3ecffc51f25afadadbb29e8b76cb
mallusrgreatv2 <freedomgamingyt@gmail.com>
2023-07-26 18:09:48 +0530

    Reload command

--- src/main/java/tk/darklegacymc/apimachine/commands/ReloadCommand.java
+++ src/main/java/tk/darklegacymc/apimachine/commands/ReloadCommand.java
@@ -1,2 +1,29 @@
-package tk.darklegacymc.apimachine.commands;public class Reload {
+package tk.darklegacymc.apimachine.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import spark.Spark;
+import tk.darklegacymc.apimachine.APIMachine;
+
+public class ReloadCommand implements CommandExecutor {
+    private APIMachine plugin;
+    public ReloadCommand(APIMachine apiMachine) {
+        plugin = apiMachine;
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(strings[0].equals("reload")) {
+            if(!commandSender.hasPermission("apimachine.reload")) {
+                commandSender.sendMessage(ChatColor.DARK_RED + "You do not have permission to use this plugin.");
+                return true;
+            }
+            plugin.reloadConfigValues();
+            commandSender.sendMessage("Reloaded APIMachine.");
+            return true;
+        }
+        return false;
+    }
 }
